d7130168c660b6ba948449da0634fb571be4a51d
Tobils <ade.suhada@mail.ugm.ac.id>
2024-01-13 17:41:50 +0700

    add terminal configuration

---
+++ lua/plugins/terminal.lua
@@ -0,0 +1,28 @@
+return {
+	"akinsho/toggleterm.nvim",
+	event = "VeryLazy",
+	opts = {
+		open_mapping = [[<c-]>]],
+		size = 50,
+		shade_terminals = true, -- NOTE: this option takes priority over highlights specified so if you specify Normal highlights you should set this to false
+		start_in_insert = true,
+		insert_mappings = true, -- whether or not the open mapping applies in insert mode
+		terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
+		persist_size = true,
+		direction = "vertical",
+	},
+	config = function(_, opts)
+		require("toggleterm").setup(opts)
+		local Terminal = require("toggleterm.terminal").Terminal
+		local lazygit = Terminal:new({
+			cmd = "lazygit",
+			hidden = true,
+			direction = "float",
+		})
+		function LazyGitToggle()
+			lazygit:toggle()
+		end
+
+		vim.api.nvim_set_keymap("n", "<leader>gk", "<cmd>lua LazyGitToggle()<CR>", { noremap = true, silent = true })
+	end,
+}
