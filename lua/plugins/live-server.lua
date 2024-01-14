c3e61adf54e786bf77eec3936f1c315541edb6e6
Tobils <ade.suhada@mail.ugm.ac.id>
2024-01-14 21:02:12 +0700

    fix liveserver

--- lua/plugins/live-server.lua
+++ lua/plugins/live-server.lua
@@ -1,3 +1,9 @@
 return {
-  "barrett-ruth/live-server.nvim",
+  require("lazy").setup({
+    {
+      "barrett-ruth/live-server.nvim",
+      build = "yarn global add live-server",
+      config = true,
+    },
+  }),
 }
