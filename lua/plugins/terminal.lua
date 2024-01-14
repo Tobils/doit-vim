6e224f4831239a42e76bc9a57b55bc8a93e95c79
Tobils <ade.suhada@mail.ugm.ac.id>
2024-01-14 20:52:32 +0700

    update terminal and liveserver

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -3,7 +3,7 @@ return {
 	event = "VeryLazy",
 	opts = {
 		open_mapping = [[<c-]>]],
-		size = 50,
+		size = 70,
 		shade_terminals = true, -- NOTE: this option takes priority over highlights specified so if you specify Normal highlights you should set this to false
 		start_in_insert = true,
 		insert_mappings = true, -- whether or not the open mapping applies in insert mode
