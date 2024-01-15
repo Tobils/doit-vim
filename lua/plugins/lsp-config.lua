af9c20bfb0f2e2f2c7b6a0106ee517bcb2c0eeea
Tobils <ade.suhada@mail.ugm.ac.id>
2024-01-15 07:28:02 +0700

    adjust lsp and terminal

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,162 +1,160 @@
 return {
-	{
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v2.x",
-		event = { "BufReadPre", "BufNewFile", "InsertEnter" },
-		dependencies = {
-			{ "neovim/nvim-lspconfig" }, -- Required
-			{
-				-- Optional
-				"williamboman/mason.nvim",
-				build = function()
-					pcall(vim.cmd, "MasonUpdate")
-				end,
-			},
-			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
-			-- java
-			-- {
-			--     "mfussenegger/nvim-jdtls",
-			-- },
-		},
-		opts = function()
-			local lsp = require("lsp-zero")
+  {
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v2.x",
+    event = { "BufReadPre", "BufNewFile", "InsertEnter" },
+    dependencies = {
+      { "neovim/nvim-lspconfig" }, -- Required
+      {
+        -- Optional
+        "williamboman/mason.nvim",
+        build = function()
+          pcall(vim.cmd, "MasonUpdate")
+        end,
+      },
+      { "williamboman/mason-lspconfig.nvim" }, -- Optional
+      -- java
+      -- {
+      --     "mfussenegger/nvim-jdtls",
+      -- },
+    },
+    opts = function()
+      local lsp = require("lsp-zero")
 
-			lsp.preset("recommended")
+      lsp.preset("recommended")
 
-			lsp.ensure_installed({
-				"vtsls",
-				"rust_analyzer",
-				"gopls",
-				"clangd",
+      lsp.ensure_installed({
+        "vtsls",
+        "rust_analyzer",
+        "gopls",
+        "clangd",
         "html",
-			})
+      })
 
-			-- Fix Undefined global 'vim'
-			lsp.nvim_workspace()
+      -- Fix Undefined global 'vim'
+      lsp.nvim_workspace()
 
-			lsp.set_preferences({
-				suggest_lsp_servers = false,
-				sign_icons = {
-					error = "E",
-					warn = "W",
-					hint = "H",
-					info = "I",
-				},
-			})
+      lsp.set_preferences({
+        suggest_lsp_servers = false,
+        sign_icons = {
+          error = "E",
+          warn = "W",
+          hint = "H",
+          info = "I",
+        },
+      })
 
-			lsp.on_attach(function(_, bufnr)
-				local opts = { buffer = bufnr, remap = false }
+      lsp.on_attach(function(_, bufnr)
+        local opts = { buffer = bufnr, remap = false }
 
-				vim.keymap.set("n", "gd", function()
-					vim.lsp.buf.definition({})
-				end, opts)
-				vim.keymap.set("n", "K", function()
-					vim.lsp.buf.hover()
-				end, opts)
-				vim.keymap.set("i", "<C-h>", function()
-					vim.lsp.buf.signature_help()
-				end, opts)
-				vim.keymap.set("n", "<leader>ff", function()
-					vim.lsp.buf.format({ async = true })
-				end, opts)
-				vim.keymap.set("n", "<leader>va", function()
-					vim.lsp.buf.code_action()
-				end, opts)
-				vim.keymap.set("n", "<leader>vf", function()
-					vim.diagnostic.open_float()
-				end, opts)
-				vim.keymap.set("n", "<leader>vn", function()
-					vim.diagnostic.goto_next()
-				end, opts)
-				vim.keymap.set("n", "<leader>vp", function()
-					vim.diagnostic.goto_prev()
-				end, opts)
-				vim.keymap.set("n", "<leader>vc", function()
-					vim.lsp.buf.rename()
-				end, opts)
-				vim.keymap.set("n", "<leader>vl", function()
-					vim.lsp.buf.references()
-				end, opts)
-			end)
+        vim.keymap.set("n", "gd", function()
+          vim.lsp.buf.definition({})
+        end, opts)
+        vim.keymap.set("n", "K", function()
+          vim.lsp.buf.hover()
+        end, opts)
+        vim.keymap.set("i", "<C-h>", function()
+          vim.lsp.buf.signature_help()
+        end, opts)
+        vim.keymap.set("n", "<leader>ff", function()
+          vim.lsp.buf.format({ async = true })
+        end, opts)
+        vim.keymap.set("n", "<leader>va", function()
+          vim.lsp.buf.code_action()
+        end, opts)
+        vim.keymap.set("n", "<leader>vf", function()
+          vim.diagnostic.open_float()
+        end, opts)
+        vim.keymap.set("n", "<leader>vn", function()
+          vim.diagnostic.goto_next()
+        end, opts)
+        vim.keymap.set("n", "<leader>vp", function()
+          vim.diagnostic.goto_prev()
+        end, opts)
+        vim.keymap.set("n", "<leader>vc", function()
+          vim.lsp.buf.rename()
+        end, opts)
+        vim.keymap.set("n", "<leader>vl", function()
+          vim.lsp.buf.references()
+        end, opts)
+      end)
 
-			lsp.setup()
+      lsp.setup()
 
-			vim.diagnostic.config({
-				virtual_text = true,
-			})
-		end,
-	},
-	{
-		"mhartington/formatter.nvim",
-		event = { "BufReadPre", "BufNewFile", "InsertEnter" },
-		config = function()
-			local function prettier()
-				return {
-					exe = "prettierd",
-					args = { vim.api.nvim_buf_get_name(0) },
-					stdin = true,
-				}
-			end
-			local function go()
-				return {
-					exe = "gofmt",
-					stdin = true,
-				}
-			end
-			local function rust()
-				return {
-					exe = "rustfmt",
-					stdin = true,
-				}
-			end
-			local function python()
-				return {
-					exe = "black",
-					args = {
-						"-q",
-						"-",
-					},
-					stdin = true,
-				}
-			end
-			local function java()
-				return {
-					exe = "clang-format",
-					args = { "--style=Google", "--assume-filename=.java" },
-					stdin = true,
-				}
-			end
-			require("formatter").setup({
-				logging = true,
-				filetype = {
-					javascript = { prettier },
-					javascriptreact = { prettier },
-					typescript = { prettier },
-					typescriptreact = { prettier },
-					css = { prettier },
-					scss = { prettier },
-					vue = { prettier },
-					json = { prettier },
-					jsonc = { prettier },
-					yaml = { prettier },
-					markdown = { prettier },
-					html = { prettier },
-					go = { go },
-					rust = { rust },
-					python = { python },
-					java = { java },
-				},
-				["*"] = {
-					require("formatter.filetypes.any").remove_trailing_whitespace,
-				},
-			})
-			vim.keymap.set("n", "<leader>fF", [[<CMD> Format<CR>]])
-			vim.keymap.set("n", "<leader>fw", [[<CMD> FormatWrite<CR>]])
+      vim.diagnostic.config({
+        virtual_text = true,
+      })
+    end,
+  },
+  {
+    "mhartington/formatter.nvim",
+    event = { "BufReadPre", "BufNewFile", "InsertEnter" },
+    config = function()
+      local function prettier()
+        return {
+          exe = "prettierd",
+          args = { vim.api.nvim_buf_get_name(0) },
+          stdin = true,
+        }
+      end
+      local function go()
+        return {
+          exe = "gofmt",
+          stdin = true,
+        }
+      end
+      local function rust()
+        return {
+          exe = "rustfmt",
+          stdin = true,
+        }
+      end
+      local function python()
+        local util = require("formatter.util")
+        return {
+          exe = "black",
+          args = { "-q", "--stdin-filename", util.escape_path(util.get_current_buffer_file_name()), "-" },
+          stdin = true,
+        }
+      end
+      local function java()
+        return {
+          exe = "clang-format",
+          args = { "--style=Google", "--assume-filename=.java" },
+          stdin = true,
+        }
+      end
+      require("formatter").setup({
+        logging = true,
+        filetype = {
+          javascript = { prettier },
+          javascriptreact = { prettier },
+          typescript = { prettier },
+          typescriptreact = { prettier },
+          css = { prettier },
+          scss = { prettier },
+          vue = { prettier },
+          json = { prettier },
+          jsonc = { prettier },
+          yaml = { prettier },
+          markdown = { prettier },
+          html = { prettier },
+          go = { go },
+          rust = { rust },
+          python = { python },
+          java = { java },
+        },
+        ["*"] = {
+          require("formatter.filetypes.any").remove_trailing_whitespace,
+        },
+      })
+      vim.keymap.set("n", "<leader>fF", [[<CMD> Format<CR>]])
+      vim.keymap.set("n", "<leader>fw", [[<CMD> FormatWrite<CR>]])
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "<leader>i", vim.lsp.buf.implementation, {})
-		end,
-	},
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>i", vim.lsp.buf.implementation, {})
+    end,
+  },
 }
--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,29 +1,29 @@
 return {
-	"akinsho/toggleterm.nvim",
-	event = "VeryLazy",
-	opts = {
-		open_mapping = [[<c-]>]],
-		size = 70,
-		shade_terminals = true, -- NOTE: this option takes priority over highlights specified so if you specify Normal highlights you should set this to false
-		start_in_insert = true,
-		insert_mappings = true, -- whether or not the open mapping applies in insert mode
-		terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
-		persist_size = true,
-		direction = "vertical",
-	},
+  "akinsho/toggleterm.nvim",
+  event = "VeryLazy",
+  opts = {
+    open_mapping = [[<c-]>]],
+    size = 70,
+    shade_terminals = true, -- NOTE: this option takes priority over highlights specified so if you specify Normal highlights you should set this to false
+    start_in_insert = true,
+    insert_mappings = true, -- whether or not the open mapping applies in insert mode
+    terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
+    persist_size = true,
+    direction = "vertical",
+  },
 
   config = function(_, opts)
-		require("toggleterm").setup(opts)
-		local Terminal = require("toggleterm.terminal").Terminal
-		local lazygit = Terminal:new({
-			cmd = "lazygit",
-			hidden = true,
-			direction = "float",
-		})
-		function LazyGitToggle()
-			lazygit:toggle()
-		end
+    require("toggleterm").setup(opts)
+    local Terminal = require("toggleterm.terminal").Terminal
+    local lazygit = Terminal:new({
+      cmd = "lazygit",
+      hidden = true,
+      direction = "float",
+    })
+    function LazyGitToggle()
+      lazygit:toggle()
+    end
 
-		vim.api.nvim_set_keymap("n", "<leader>gk", "<cmd>lua LazyGitToggle()<CR>", { noremap = true, silent = true })
-	end,
+    vim.api.nvim_set_keymap("n", "<leader>gk", "<cmd>lua LazyGitToggle()<CR>", { noremap = true, silent = true })
+  end,
 }
